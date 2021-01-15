71f41e7874171fbbec6060c101e04bb6b8c62135
PVillela <pvillela@gmail.com>
2021-01-15 15:11:07 -0500

    Split collections/types.go into 3 separate files.

--- pkg/collections/type_aliases.go
+++ pkg/collections/type_aliases.go
@@ -9,18 +9,15 @@ type Any = T0
 
 // T1 is a type alias.
 // Used to clarify method signatures and facilitate replacement for code generation.
-type T1 = Any
+type T1 = T0
 
 // T2 is a type alias.
 // Used to clarify method signatures and facilitate replacement for code generation.
-type T2 = Any
+type T2 = T0
 
 // T3 is a type alias.
 // Used to clarify method signatures and facilitate replacement for code generation.
-type T3 = Any
-
-// SliceT0 is a type wrapper, implements List interface.
-type SliceT0 []T0
+type T3 = T0
 
 // SliceAny is a type alias.
 type SliceAny = SliceT0
@@ -33,26 +30,11 @@ type SliceT1 = SliceT0
 // Used to clarify method signatures and facilitate replacement for code generation.
 type SliceT2 = SliceT0
 
-// Slice2T0 is a type wrapper.
-type Slice2T0 []SliceT0
-
-// PairT0T1 defines a pseudo-generic pair.
-type PairT0T1 struct {
-	X1 T0
-	X2 T1
-}
-
 // PairAnyAny is a type alias.
 type PairAnyAny = PairT0T1
 
-// SliceOfPairT0T1 is a type wrapper.
-type SliceOfPairT0T1 []PairT0T1
-
 // SliceOfPairAnyAny is a type alias.
 type SliceOfPairAnyAny = SliceOfPairT0T1
-
-// MapT0T1 is a type wrapper, implements Map interface.
-type MapT0T1 map[T0]T1
 
 // MapAnyAny is a type alias.
 type MapAnyAny = MapT0T1
@@ -63,23 +45,14 @@ type MapT0T2 = MapT0T1
 // MapT2T1 is a alias.
 type MapT2T1 = MapT0T1
 
-// MapT1SliceT0 is a type wrapper.
-type MapT1SliceT0 map[T1]SliceT0
-
 // MapAnySliceAny is a type alias.
 type MapAnySliceAny = MapT1SliceT0
-
-// SetT0 is a type wrapper, implements Set interface.
-type SetT0 map[T0]bool
 
 // SetAny is a type alias.
 type SetAny = SetT0
 
 // SetT1 is a type alias.
 type SetT1 = SetT0
-
-// SetOfPairT0T1 is a type wrapper, implements Set interface.
-type SetOfPairT0T1 map[PairT0T1]bool
 
 // SetOfPairAnyAny is a type alias.
 type SetOfPairAnyAny = SetOfPairT0T1
---
+++ pkg/collections/types_0.go
@@ -0,0 +1,10 @@
+package collections
+
+// SliceT0 is a type wrapper, implements List interface.
+type SliceT0 []T0
+
+// Slice2T0 is a type wrapper.
+type Slice2T0 []SliceT0
+
+// SetT0 is a type wrapper, implements Set interface.
+type SetT0 map[T0]bool
